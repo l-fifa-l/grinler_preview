4358ea1f1e673df3c692759d9cd78ffdea952db8
Vivek Kumar <starktestic@gmail.com>
2023-01-30 18:37:53 +0530

    Update Front.jsx

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Front() {
   return (
     <section className="w-full">
-      <div className="w-full px-10 py-4 mx-auto">
+      <div className="w-full px-10 py-4 mx-auto" contentEditable="true">
         <div className="w-full mx-auto text-left md:text-center">
           <h1 className="mb-6 text-4xl font-extrabold leading-none max-w-5xl mx-auto tracking-normal text-gray-900 sm:text-6xl md:text-6xl lg:text-7xl md:tracking-tight">
             <span className="w-full text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-500 lg:inline">
