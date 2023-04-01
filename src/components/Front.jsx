83744204af9a80a550853762e57ef0ac95221d86
Vivek Kumar <starktestic@gmail.com>
2023-04-01 23:08:48 +0530

    Update Front.jsx

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -65,7 +65,7 @@ function Front() {
 
             <div className="flex items-center gap-x-3 sm:text-sm">
               <a
-                href="https://app.grinler.com/"
+                href="https://meemit.online/"
                 className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 duration-150 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
               >
                 Open Web-App
