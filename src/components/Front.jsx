ac8172d420492f88d08af11a45129d553c81f35c
Vivek Kumar <starktestic@gmail.com>
2023-06-11 22:27:36 +0530

    Update Front.jsx

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -2,14 +2,18 @@
 
 import React from 'react';
 import Waitlist from './Waitlist';
+import Link from 'next/link';
 
 function Front() {
   return (
     <div className="flex flex-col justify-evenly ju max-w-screen px-4 pt-4 mx-auto lg:pt-16 lg:flex-row md:px-8 lg:max-w-screen bg-gradient-to-b from-[#00D3CA] via-[#ffffff] to-white ">
       <div className="pt-16  md:mb-16 lg:mb-0 lg:pt-16 lg:max-w-lg">
-        <div className="max-w-xl mb-3 sm:mt-4 lg:space-y-6">
-          <div>
-            <div className="inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white">
+        <div className="max-w-xl my-8 sm:mt-4 lg:space-y-6">
+          <div className="mt-12">
+            <Link
+              href={'#download'}
+              className="inline-flex gap-x-6 items-center rounded-full p-1 pr-6 border text-sm font-medium duration-150 hover:bg-white"
+            >
               <span className="inline-block rounded-full px-3 py-1 bg-indigo-600 text-white">
                 Update
               </span>
@@ -28,31 +32,31 @@ function Front() {
                   />
                 </svg>
               </p>
-            </div>
+            </Link>
           </div>
 
-          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+          <h2 className="max-w-lg my-6   font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
             AI Platform designed for <br className="hidden md:block" />
             meme lovers.{' '}
-            <span className="inline-block text-deep-purple-accent-400">
+            {/* <span className="inline-block text-deep-purple-accent-400">
               Show your funny side
-            </span>
+            </span> */}
           </h2>
-          <p className="text-base text-gray-700 md:text-lg">
+          {/* <p className="text-base text-gray-700 md:text-lg">
             Powering humor and creativity through meme collaboration laugh
             ,create, share, and connect.
-          </p>
-          <h1 className="text-5xl font-extrabold lg:text-7xl 2xl:text-5xl">
+          </p> */}
+          <h1 className="text-5xl mb-10 font-extrabold lg:text-7xl 2xl:text-5xl">
             <span className="text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500 dark:from-teal-200 dark:via-indigo-300 dark:to-sky-500">
-              Coming
+              Coming{' '}
             </span>
 
             <span className="text-transparent bg-gradient-to-tr bg-clip-text from-blue-500 via-pink-500 to-red-500 dark:from-sky-300 dark:via-pink-300 dark:to-red-500">
               Soon
             </span>
           </h1>
-          <Waitlist />
-          <div className="flex md:items-center sm:justify-start">
+          <Waitlist className="my-12" />
+          <div className="flex my-6 md:items-center sm:justify-start">
             <div className="flex justify-evenly space-x-8 lg:space-x-10 pt-6">
               <img
                 className="h-14 "
